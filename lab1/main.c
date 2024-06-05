86f4635517321142e28e93d0e36b2456ea38b635
Коротыкина Анастасия Александровна <103072332+Searchforsomething@users.noreply.github.com>
2024-06-05 19:21:22 +0300

    Add files via upload

--- lab1/main.c
+++ lab1/main.c
@@ -7,8 +7,12 @@
 #include <unistd.h>
 #include <getopt.h>
 
+// Глобальная переменная для хранения искомой последовательности байтов
 const char *target_bytes;
+// Переменная для включения режима отладки
+int debug_mode = 0;
 
+// Прототипы функций
 void print_help();
 void print_version();
 int search_in_file(const char *file_path);
@@ -18,12 +22,20 @@ int main(int argc, char *argv[]) {
     int option_index = 0;
     int c;
 
+
+    // Определение длинных и коротких опций для getopt_long
     struct option long_options[] = {
         {"help", no_argument, 0, 'h'},
         {"version", no_argument, 0, 'v'},
         {0, 0, 0, 0}
     };
 
+    // Проверка наличия переменной окружения LAB11DEBUG для включения режима отладки
+    if (getenv("LAB11DEBUG") != NULL) {
+        debug_mode = 1;
+    }
+
+    // Обработка опций командной строки
     while ((c = getopt_long(argc, argv, "hv", long_options, &option_index)) != -1) {
         switch (c) {
             case 'h':
@@ -33,22 +45,25 @@ int main(int argc, char *argv[]) {
                 print_version();
             return EXIT_SUCCESS;
             case '?':
-                // getopt_long already printed an error message.
                     return EXIT_FAILURE;
             default:
                 abort();
         }
     }
 
+    // Проверка наличия обязательных аргументов <директория> и <последовательность байтов>
     if (optind + 2 > argc) {
         fprintf(stderr, "Использование: ./lab11aakN3250 [опции] <директория> <последовательность байтов>\n");
         return EXIT_SUCCESS;
     }
 
+    // Получение значений аргументов
     const char *dir_path = argv[optind];
     target_bytes = argv[optind + 1];
+    // Запуск рекурсивного обхода файловой системы с использованием nftw
     int res = nftw(dir_path, process_file, 20, FTW_PHYS);
 
+    // Проверка на ошибки выполнения nftw
     if (res < 0) {
         perror("nftw");
         return EXIT_FAILURE;
@@ -57,6 +72,7 @@ int main(int argc, char *argv[]) {
     return EXIT_SUCCESS;
 }
 
+// Функция для вывода справки по использованию программы
 void print_help() {
     printf("Использование: ./lab11aakN3250 [опции] <директория> <последовательность байтов>\n");
     printf("Программа выполняет рекурсивный поиск заданной последовательности файлов.\n");
@@ -65,11 +81,13 @@ void print_help() {
     printf("  -v, --version  Вывод информации о версии\n");
 }
 
+// Функция для вывода информации о версии программы
 void print_version() {
     printf("lab11aakN3250 v1.0\n");
     printf("Автор: Коротыкина Анастасия Александровна, N3250 \nВариант 4\n");
 }
 
+// Функция для поиска заданной последовательности байтов в файле
 int search_in_file(const char *file_path) {
     FILE *file = fopen(file_path, "rb");
     if (file == NULL) {
@@ -77,10 +95,12 @@ int search_in_file(const char *file_path) {
         return 0;
     }
 
+    // Определение размера файла
     fseek(file, 0, SEEK_END);
     long file_size = ftell(file);
     rewind(file);
 
+    // Выделение памяти для чтения файла и дополнительного нулевого символа
     char *buffer = (char *)malloc(file_size + 1);
     if (buffer == NULL) {
         perror("malloc");
@@ -88,15 +108,27 @@ int search_in_file(const char *file_path) {
         return 0;
     }
 
+    // Чтение содержимого файла в буфер
     size_t readsize = fread(buffer, 1, file_size, file);
-    buffer[readsize] = '\0';
+    buffer[readsize] = '\0'; // Добавление нулевого символа в конец буфера
     fclose(file);
 
+    // Поиск заданной последовательности байтов в буфере
     int found = (strstr(buffer, target_bytes) != NULL);
+    // Вывод отладочной информации, если включен режим отладки
+    if (debug_mode) {
+        if (found) {
+            fprintf(stderr, "Found target bytes in file %s", file_path);
+        }
+        else {
+            fprintf(stderr, "Target bytes not found in file %s\n", file_path);
+        };
+    };
     free(buffer);
     return found;
 }
 
+// Функция для обработки каждого файла, найденного nftw
 int process_file(const char *path, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
     if (typeflag == FTW_F) {
         if (search_in_file(path)) {
